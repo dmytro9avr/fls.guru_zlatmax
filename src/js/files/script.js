a323017daf5a46ed151f74dce0b07e6d853a2d87
DiA <dimasssa91@gmail.com>
2022-01-11 21:59:31 +0200

    3.30

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,3 +1,22 @@
 // Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
 // import { isMobile } from "./functions.js";
 // import { formsModules } from "./forms/forms.js";
+
+document.addEventListener("click", documentActions);
+
+function documentActions(e) {
+   const targetElement = e.target ;
+   if (targetElement.closest('[data-parent]')) {
+      const subMenuId = targetElement.dataset.parent ? targetElement.dataset.parent : null; 
+      const subMenu = document.querySelector(`[data-submenu="${subMenuId}"]`);
+      if (subMenu) {
+         targetElement.classList.toggle('_sub-menu-active');
+         subMenu.classList.toggle('_sub-menu-open');
+
+      } else {
+         console.log("sub Menu empty")
+      }
+      e.preventDefault();
+   }
+
+};
