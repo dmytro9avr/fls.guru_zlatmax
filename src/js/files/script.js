a0c640cf0d089e6a7c6bc2729b0b1c966b54bbd0
DiA <dimasssa91@gmail.com>
2022-01-12 21:24:05 +0200

    3.40 stop on fix text in header of menu

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -9,7 +9,14 @@ function documentActions(e) {
    if (targetElement.closest('[data-parent]')) {
       const subMenuId = targetElement.dataset.parent ? targetElement.dataset.parent : null; 
       const subMenu = document.querySelector(`[data-submenu="${subMenuId}"]`);
+      const catalogMenu = document.querySelector('.catalog-header');
       if (subMenu) {
+        const activeLink = document.querySelector('._sub-menu-active');
+         const activeBlock = document.querySelector('._sub-menu-open');
+            if (activeLink && activeLink !== targetElement) {
+               activeLink.classList.remove('_sub-menu-active');
+               activeBlock.classList.remove('_sub-menu-open');
+               } 
          targetElement.classList.toggle('_sub-menu-active');
          subMenu.classList.toggle('_sub-menu-open');
 
